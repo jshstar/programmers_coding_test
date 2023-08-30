0a92cc2fb46905890a2e5292e4bfbc17d58209ea
jshstar <jshstar97@gmail.com>
2023-08-30 20:30:31 +0900

    Lv1 coding-test solve 20230830

---
+++ untitled/src/Lv1/javatest25.java
@@ -0,0 +1,33 @@
+package Lv1;
+
+public class javatest25 {
+    public static void main(String[] args) {
+        int[] num = {1,2,7,6,4};
+        solution(num);
+    }
+    public static int solution(int[] nums) {
+        int answer = 0;
+        int sum;
+        boolean flag;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i+1; j <nums.length ; j++) {
+                for (int k = j+1; k < nums.length; k++) {
+                    sum = nums[i]+nums[j]+nums[k];
+                    flag =true;
+                    for(int l=2;l<sum;l++){
+                        if( sum%l == 0 ) {
+                            flag = false;
+                            break;
+                        }
+                        else flag =true;
+                    }
+                    if(flag)
+                        answer++;
+                }
+            }
+        }
+        return answer;
+    }
+
+
+}
---
+++ untitled/src/Lv1/javatest26.java
@@ -0,0 +1,21 @@
+package Lv1;
+
+public class javatest26 {
+    public static void main(String[] args) {// 덧칠하기
+        int[] section = {2,3,6};
+        solution(8,4,section);
+    }
+    public static int solution(int n, int m, int[] section)
+    {
+        int answer = 0;
+        int start = section[0];
+        answer++;
+        for (int point: section) {
+            if(start + m > point)
+                continue; // 페인트칠 범위에 없으면 패스
+            start = point;
+            answer++;
+        }
+        return answer;
+    }
+}
