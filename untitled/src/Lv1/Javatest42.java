ee9bc7a63066edf5550ae25433142b1525b32642
jshstar <jshstar97@gmail.com>
2023-11-09 10:46:51 +0900

    2023-11-09 update

--- untitled/src/Lv1/Javatest42.java
+++ untitled/src/Lv1/Javatest42.java
@@ -12,26 +12,26 @@ public class Javatest42 { // 크레인 인형뽑기 게임
     }
     public int solution(int[][] board, int[] moves) {
         int answer = 0;
-        Stack<Integer> stack = new Stack();
-        for (int i = 0; i < moves.length; i++) {
-            for (int j = 0; j < board.length; j++) {
-                if(board[j][moves[i]-1] !=0)
+        Stack<Integer> stack = new Stack(); // 스텍생성
+        for (int i = 0; i < moves.length; i++) { // 이동 순회
+            for (int j = 0; j < board.length; j++) { // 이동한 위치에서 인형 뽑는 과정
+                if(board[j][moves[i]-1] !=0) // 뽑는 인형이 있을때까지
                 {
-                    if(!stack.isEmpty())
+                    if(!stack.isEmpty()) // 뽑은 인형 저장공간이 비어있지 않으면
                     {
-                        if(stack.peek() == board[j][moves[i]-1])
+                        if(stack.peek() == board[j][moves[i]-1]) // 이미 쌓여 있는 인형이 같은 인형인지 체크
                         {
-                            stack.pop();
-                            answer +=2;
+                            stack.pop(); // 같은 인형 제거
+                            answer +=2; // 제거된 인형수 만큼 +2
                         }
-                        else stack.push(board[j][moves[i]-1]);
+                        else stack.push(board[j][moves[i]-1]); // 같지 않으면 추가
                     }
-                    else stack.push(board[j][moves[i]-1]);
-                    board[j][moves[i]-1] =0;
-                    break;
+                    else stack.push(board[j][moves[i]-1]); // 비어있으면 추가
+                    board[j][moves[i]-1] =0; // 뽑은 인형자리는 0으로 빈자리 처리
+                    break;// 뽑은후 탐색 break
                 }
             }
         }
-        return answer;
+        return answer; // 뽑아서 처리한 인형 총 개수
     }
 }
