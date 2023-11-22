609a82fa794f3f538e6a7e53ab51374fed498de4
jshstar <jshstar97@gmail.com>
2023-11-22 10:12:04 +0900

    2023-11-22 update

--- untitled/src/Lv1/Javatest59.java
+++ untitled/src/Lv1/Javatest59.java
@@ -1,15 +1,18 @@
 package Lv1;
 
 public class Javatest59 { // 내적
-    public static void main(String[] args) {
-        Javatest59 javatest59 = new Javatest59();
-        javatest59.solution(new int[]{1,2,3,4}, new int[]{-3,-1,0,2});
-    }
-    public int solution(int[] a, int[] b) {
-        int answer = 0;
-        for (int i = 0; i < a.length; i++) {
-            answer += a[i]*b[i];
-        }
-        return answer;
-    }
+	public static void main(String[] args) {
+		Javatest59 javatest59 = new Javatest59();
+		javatest59.solution(new int[] {1, 2, 3, 4}, new int[] {-3, -1, 0, 2});
+	}
+
+	public int solution(int[] a, int[] b) {
+		int answer = 0;
+
+		for (int i = 0; i < a.length; i++) {
+			answer += a[i] * b[i];
+		}
+
+		return answer;
+	}
 }
---
+++ untitled/src/Lv1/Javatest61.java
@@ -0,0 +1,22 @@
+package Lv1;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Javatest61 { // 문자열 내림차순으로 배치하기
+	public static void main(String[] args) {
+		Javatest61 javatest61 = new Javatest61();
+		javatest61.solution("Zbcdefg");
+	}
+	public String solution(String s) {
+		String answer="";
+		String[] sArr = s.split("");
+		Arrays.sort(sArr, Collections.reverseOrder());
+		for (String a: sArr) {
+			answer += a;
+		}
+		return answer;
+	}
+
+
+}
