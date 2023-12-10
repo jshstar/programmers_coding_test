5414eff75727c8316bf919d73e8319bdc1ec5931
jshstar <jshstar97@gmail.com>
2023-12-10 22:51:43 +0900

    2023-12-10

---
+++ untitled/src/Lv1/Javatest70.java
@@ -0,0 +1,20 @@
+package Lv1;
+
+import java.util.HashMap;
+
+public class Javatest70 { // 신고 결과 받기
+	public static void main(String[] args) {
+
+	}
+	public int[] solution(String[] id_list, String[] report, int k) {
+		int[] answer = {};
+		for (int i = 0; i < report.length; i++) {
+			String[] reportName = report[i].split(" ");
+			String complainant = reportName[0];
+			String blackName = reportName[1];
+
+
+		}
+		return answer;
+	}
+}
