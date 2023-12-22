999421cd0e7b28f9fe4639e735d147d9d9f75222
jshstar <jshstar97@gmail.com>
2023-12-22 12:59:03 +0900

    2023-12-22

--- untitled/src/Lv2/Javatest10.java
+++ untitled/src/Lv2/Javatest10.java
@@ -1,2 +1,22 @@
-package Lv2;public class Javatest10 {
+package Lv2;
+
+public class Javatest10 {
+	public static void main(String[] args) {
+		Javatest10 javatest10 = new Javatest10();
+		javatest10.solution(8,4,7);
+	}
+	public int solution(int n, int a, int b)
+	{
+		int answer = 0;
+
+		while(true){
+			a = a/2 + a%2;
+			b = b/2 + b%2;
+			answer++;
+			if(a==b){
+				break;
+			}
+		}
+		return answer;
+	}
 }
--- untitled/src/Lv2/Javatest11.java
+++ untitled/src/Lv2/Javatest11.java
@@ -1,2 +1,33 @@
-package Lv2;public class Javatest11 {
+package Lv2;
+
+public class Javatest11 {
+	public static void main(String[] args) {
+		Javatest11 javatest11 = new Javatest11();
+		javatest11.solution(new int[]{2,6,8,14});
+	}
+	public int solution(int[] arr) {
+		int answer = 0;
+		if(arr.length == 1){
+			return arr[0];
+		}
+		int g = gcd(arr[0], arr[1]);
+		answer = (arr[0] * arr[1]) /g;
+
+		if(arr.length >2){
+			for (int i = 0; i < arr.length; i++) {
+				g= gcd(answer, arr[i]);
+				answer = (answer * arr[i])/g;
+			}
+		}
+		return answer;
+
+	}
+	public static int gcd(int a, int b){
+		int r = a%b;
+		if(r== 0 ) {
+			return b;
+		} else {
+			return gcd(b,r);
+		}
+	}
 }
